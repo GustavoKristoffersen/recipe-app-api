ce8e65af9c2f00fa17439ed5c02d8c4547bc91f4
Gustavo William <gustavowilliam0980@gmail.com>
2022-02-06 23:14:43 -0300

    Fix password short test

--- app/users/tests/test_users_api.py
+++ app/users/tests/test_users_api.py
@@ -22,11 +22,7 @@ class PublicUserApiTests(TestCase):
 
     def test_create_valid_user_succeed(self):
         """Test creating users with valid data should succeed"""
-        payload = {
-            'email': 'gustavo@test.com',
-            'password': '123456',
-            'name': 'Gustavo',
-        }
+        payload = {'email': 'gustavo@test.com', 'password': '123456', 'name': 'Gustavo'}
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
@@ -44,7 +40,7 @@ class PublicUserApiTests(TestCase):
 
     def test_create_user_with_short_password_fails(self):
         """Test user password must be at least 5 characters"""
-        payload = {'email': 'gustavo@test.com', 'password': '123'}
+        payload = {'email': 'gustavo@test.com', 'password': '123', 'name': 'Gustavo'}
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
